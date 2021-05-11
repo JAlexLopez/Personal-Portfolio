90f744ad709b26a781b204dd0e46d35cee2140e0
Alex <jalexlopez15@gmail.com>
2021-05-10 17:24:17 -0700

    Finished updated site

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-import React, {useState, useRef, useEffect} from 'react';
-import './style/FadeInSection.css';
+function toggleMenu() {
+    var links = document.getElementById('nav-links'),
+        container = document.getElementById('nav-container');
 
-export default function FadeInSection(props) {
-  const [isVisible, setVisible] = useState(true);
-
-  const domRef = React.useRef();
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => setVisible(entry.isIntersecting));
-    });
-
-    observer.observe(domRef.current);
-
-    return () => observer.unobserve(domRef.current);
-  }, []);
-
-  return (
-    <div ref={ domRef } className={ `fade-in-section ${ isVisible ? 'is-visible' : '' }` }>
-      { props.children }
-    </div>
-  )
+    if (links.style.display === "flex") {
+        links.style.display = "none";
+        container.style.backgroundColor = 'transparent';
+    } else {
+        links.style.display = 'flex';
+        links.classList.add('active-menu');
+        links.classList.add('fade-in');
+        container.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    }
 }
